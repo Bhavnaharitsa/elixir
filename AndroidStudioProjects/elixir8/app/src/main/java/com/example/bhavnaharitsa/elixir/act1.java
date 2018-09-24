e5bc33cd67d7b3917c576f891bd96c77562e2f81
BHAVNA HARITSA <bhavnaharitsa@gmail.com>
2018-09-24 20:32:13 +0530

    First app

---
+++ AndroidStudioProjects/elixir8/app/src/main/java/com/example/bhavnaharitsa/elixir/act1.java
@@ -0,0 +1,29 @@
+package com.example.bhavnaharitsa.elixir;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class act1 extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act1);
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(act1.this,act2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+
+
